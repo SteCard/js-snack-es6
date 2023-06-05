8894bf0d9ae786ac0efd98b80cd45c31aa3a1b5f
SteCard <stefanocardillo14@gmail.com>
2023-06-05 09:48:13 +0200

    snack 1

--- SNACK 1/script.js
+++ SNACK 1/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const guests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin' ];
+
+console.log(guests);
+
+const invitation_array = guests.map((guest, i) => {
+    return { '_nome_del_tavolo_': 'Tavolo Vip', 'nome_dell_ospite_': guest, '_posto_occupato_': i}
+});
+
+console.log(invitation_array)
