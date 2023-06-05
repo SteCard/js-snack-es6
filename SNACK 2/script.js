ad65890a664df855a5c7a007835281ac6b2f6fcc
SteCard <stefanocardillo14@gmail.com>
2023-06-05 09:56:44 +0200

    first array

--- SNACK 2/script.js
+++ SNACK 2/script.js
@@ -1,1 +1,47 @@
 "use strict";
+
+const students = [
+    {
+        'id': '213',
+        'name': 'Marco della Rovere',
+        'grades': '78'
+    },
+    {
+        'id': '110',
+        'name': 'Paola Cortellessa',
+        'grades': '96'
+    },
+    {
+        'id': '250',
+        'name': 'Andrea Mantegna',
+        'grades': '48'
+    },
+    {
+        'id': '145',
+        'name': 'Gaia Borromini',
+        'grades': '74'
+    },
+    {
+        'id': '196',
+        'name': 'Luigi Grimaldello',
+        'grades': '68'
+    },
+    {
+        'id': '102',
+        'name': 'Piero della Francesca',
+        'grades': '50'
+    },
+    {
+        'id': '120',
+        'name': 'Francesca da Polenta',
+        'grades': '84'
+    },
+]
+
+console.log(students);
+
+// Restituisco ARRAY con i nomi trasformati in Maiuscolo
+const plates = students.map((student) => student.name.toUpperCase());
+
+console.log(plates);
+
